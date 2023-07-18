3fc2bb5c162d3df82811e121a1fcc703ee6600fa
jnggeun <103514126+reg22222@users.noreply.github.com>
2023-07-18 20:30:34 +0900

    Add files via upload

---
+++ myboard/src/main/java/tommy/spring/polymorphism/LgTV.java
@@ -0,0 +1,36 @@
+package tommy.spring.polymorphism;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("tv")
+public class LgTV implements TV {
+
+	@Autowired
+	private Speaker speaker;
+	
+	public LgTV() {
+		System.out.println("LgTV °´Ã¼ »ý¼º");
+	}
+
+	@Override
+	public void powerOn() {
+		System.out.println("LgTV Àü¿øÀ» ÄÒ´Ù.");
+	}
+
+	@Override
+	public void powerOff() {
+		System.out.println("LgTV Àü¿øÀ» ²ö´Ù.");
+	}
+
+	@Override
+	public void volumeUp() {
+		speaker.volumeUp();
+	}
+
+	@Override
+	public void volumeDown() {
+		speaker.volumeDown();
+	}
+
+}
---
+++ myboard/src/main/java/tommy/spring/polymorphism/SamsungTV.java
@@ -0,0 +1,56 @@
+package tommy.spring.polymorphism;
+
+public class SamsungTV implements TV{
+	
+	private Speaker speaker;
+	private int price;
+	
+	public SamsungTV() {
+		System.out.println("SamsungTV 객체 생성");
+	}
+
+	public SamsungTV(Speaker speaker) {
+		System.out.println("SamsungTV 객체 생성 : 생성자 인젝션");
+		this.speaker = speaker;
+	}
+
+	public SamsungTV(Speaker speaker, int price) {
+		System.out.println("SamsungTV 객체 생성 : 생성자 인젝션 - 다중매핑");
+		this.speaker = speaker;
+		this.price = price;
+	}
+
+	public void powerOn() {
+		System.out.println("SamsungTV 전원을 켠다. (가격 : " + price + "원)");
+	}
+
+	public void powerOff() {
+		System.out.println("SamsungTV 전원을 끈다.");
+	}
+
+	public void volumeUp() {
+		speaker.volumeUp();	
+	}
+
+	public void volumeDown() {
+		speaker.volumeDown();
+	}
+
+	public Speaker getSpeaker() {
+		return speaker;
+	}
+
+	public void setSpeaker(Speaker speaker) {
+		System.out.println("setSpeaker() 메서드 호출");
+		this.speaker = speaker;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		System.out.println("setPrice() 메서드 호출");
+		this.price = price;
+	}
+}
